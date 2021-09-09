82937363cac5cf73b984b9ec3fa79ce82f8d56f5
myungsworld <myungsworld@gmail.com>
2021-09-09 11:22:29 +0900

    add gorm and READEME update

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module myungsworld
 
 go 1.16
+
+require (
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	gorm.io/driver/mysql v1.1.2
+	gorm.io/gorm v1.21.14
+)
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import "myungsworld/api/bithumb/Info"
+import (
+	Execute "myungsworld/api/bithumb/transaction"
+	"myungsworld/database"
+)
 
 func main() {
 
+	database.ConnectDB()
 
 	//Info.CoinMarketCondition("BTT")
 	//Info.GetMyTickerBalance("BTT","KRW")
@@ -16,5 +20,6 @@ func main() {
 	//Execute.SellHook("BTT","500","6")
 	//Execute.MarKetBuy("BTT","2")
 	//Execute.MarketSell("BTT","1")
-	Info.CandleStick("BTT","24h")
+	//Info.CandleStick("BTT","24h")
+	Execute.MarketBuyHook("BTT","4","4","1500")
 }
