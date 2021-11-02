8a4fcb2014833d6c300208c389efd3f244bc88b5
myungsworld <myungsworld@gmail.com>
2021-11-02 18:00:39 +0900

    set coin default config

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"myungsworld/database"
 	Queue "myungsworld/queue"
+	"time"
 )
 
 // Ticker : 코인티커 BasicCycle : 기본 대기 시간
@@ -29,19 +30,10 @@ func main() {
 	loop := make(chan bool, 1)
 
 	list := make([]Config, 0)
-
-	list = append(list,
-		Config{
-			Ticker: "BTT", BasicCycle: 600,
-			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
-			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
-		}, Config{
-			Ticker: "MIX", BasicCycle: 600,
-			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
-			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
-		})
+	list = setCoinDefaultConfig(list)
 
 	for _, config := range list {
+		time.Sleep(time.Millisecond*250)
 		go Queue.Coin(
 			config.Ticker, config.BasicCycle,
 			config.PercentCrashing, config.PercentSecondCrashing, config.CrashedCycle,
@@ -53,4 +45,119 @@ func main() {
 		<-loop
 	}
 
+}
+
+func setCoinDefaultConfig(list []Config) []Config {
+
+
+	// order by opening price desc
+	list = append(list,
+		Config{
+			Ticker: "BNB", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		}, Config{
+			Ticker: "KSM", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "COMP", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "AAVE", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "SOL", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "LTC", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "BSV", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "AXS", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "BTG", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "ETC", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "DOT", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "NMR", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "LUNA", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "ATOM", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "LINK", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "LPT", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "BAL", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "WAVES", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "XVS", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "UNI", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "REP", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "CAKE", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "QTUM", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "ALICE", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "OMG", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},Config{
+			Ticker: "SUSHI", BasicCycle: 600,
+			PercentCrashing: -3, PercentSecondCrashing: -5, CrashedCycle: 600,
+			PercentSoaring: 3, PercentFirstSell: 10, PercentSecondSell: 20, PercentLastSell: 30, SoaringCycle: 1200,
+		},
+		)
+
+	return list
 }
